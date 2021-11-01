cf82d8c393879133cb935ef551917004c4153268
Rahul Sil <rahul.official.150@gmail.com>
2021-11-02 03:26:00 +0530

    php page

---
+++ lw.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<pre>
+
+<?php
+
+echo "<body style='background-color:pink'>";
+
+echo "<h1>Welcome to Rahul Webserver !!</h1>\n<h3>";
+
+echo `/usr/sbin/ifconfig`;
+?>
+
+</pre>
+</html>
